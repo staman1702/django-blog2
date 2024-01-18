e535b0279f063cc342f40e064d6765beeb9c41a0
staman1702 <>
2024-01-18 20:39:50 +0000

    migrate comments

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
-class Comment(models):
+class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
